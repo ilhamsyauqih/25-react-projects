86dde8219bcc1936ab0248b86596dde4b9793df2
Ilham Syauqi <ilhamsyauqih@outlook.co.id>
2026-08-26 23:02:06 +0700

    update mencoba accordian sendiri dan menambahkan random color

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,62 +1,57 @@
-//Single Selection
-//Multiple Selection
+import { use, useState } from 'react';
+import data from './data'
+import './styles.css'
 
-import { useState } from "react"
-import data from "./data";
-import "./styles.css";
+const Accordian = () => {
 
-export default function Accordian() {
-
-    const [selected, setSelected] = useState(null);
+    const [selected, setSelected] = useState(null)
     const [enableMultiSelection, setEnableMultiSelection] = useState(false);
     const [multiple, setMultiple] = useState([]);
 
-    const handleSingleSelection = (getCurrentId) => {
-        setSelected(getCurrentId === selected ? null : getCurrentId)
+    const handleSingleSelect = (getId) => {
+        setSelected(getId === selected ? null : getId)
     }
 
-    const handleMultiSelection = (getCurrentId) => {
+    const handleMultipleSelect = (getId) => {
         let cpyMultiple = [...multiple];
-        const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId)
+
+        const findIndexOfCurrentId = cpyMultiple.indexOf(getId);
         
-        console.log(findIndexOfCurrentId);
-        
-        if(findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId)
+        if(findIndexOfCurrentId === -1) cpyMultiple.push(getId)
         else cpyMultiple.splice(findIndexOfCurrentId, 1)
 
         setMultiple(cpyMultiple)
     }
-
-    console.log(selected, multiple);
-
+    
 
     return <div className="wrapper">
-        <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
+        <button onClick={()=>setEnableMultiSelection(!enableMultiSelection)}>Multiple Select</button>
         <div className="accordian">
             {
                 data && data.length > 0 ?
-                    data.map((dataItem) => (
-                        <div className="item">
-                            <div onClick={enableMultiSelection 
-                                ? () => handleMultiSelection(dataItem.id) 
-                                : () => handleSingleSelection(dataItem.id)} className="title">
-                                <h3>{dataItem.question}</h3>
-                                <span>+</span>
-                            </div>
-                            {
-                                enableMultiSelection ?
-                                multiple.indexOf(dataItem.id) !== -1 && <div className="content">{dataItem.answer}</div> :
-                                selected === dataItem.id && <div>{dataItem.answer}</div>
-                            }
-                            {/* {
-                                selected === dataItem.id ?
-                                    <div>{dataItem.answer}</div>
-                                    : null
-                            } */}
+                data.map((dataItem)=>(
+                    <div className='item'>
+                        <div onClick={
+                            enableMultiSelection ? 
+                            ()=>handleMultipleSelect(dataItem.id) :
+                            ()=>handleSingleSelect(dataItem.id)} className='title'>
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
                         </div>
-                    ))
-                    : <div>No data found!</div>
+                        {
+                            enableMultiSelection ?
+                            multiple.indexOf(dataItem.id) !== -1 && <div>{dataItem.answer}</div> :
+                            selected === dataItem.id && <div>{dataItem.answer}</div>
+                        }
+                        {/* {
+                            selected === dataItem.id ?
+                            <div>{dataItem.answer}</div> : null
+                        } */}
+                    </div>
+                )) : <div>Data not found</div>
             }
         </div>
     </div>
 }
+
+export default Accordian;
