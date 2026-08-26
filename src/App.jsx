9519c3a45bf8bd0662c9086ed59aa90fd73b2f01
Ilham Syauqi <ilhamsyauqih@outlook.co.id>
2026-08-26 17:58:09 +0700

    menambahkan components accordion

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Accordian from './components/accordion'
+
+function App() {
+
+  return (
+    <>
+      {/* Accordian Component */}
+      <Accordian/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/accordion/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: '1',
+    question: 'What are accordion components?',
+    answer:
+      'Accordion components are user interface elements used for organizing and displaying content in a compact way.',
+  },
+  {
+    id: '2',
+    question: 'What are they used for?',
+    answer:
+      'They are commonly employed in various contexts, including FAQs, menus, and content sections.',
+  },
+  {
+    id: '3',
+    question: 'Accordion as a musical instrument',
+    answer:
+      'The accordion is a musical instrument with a keyboard and bellows.',
+  },
+  {
+    id: '4',
+    question: 'Can I create an accordion component with a different framework?',
+    answer:
+      'Yes, of course, it is very possible to create an accordion component using different frameworks.',
+  },
+];
+
+export default data;
