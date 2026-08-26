576afe1e041b9a0d7dcb88820a4a235e07ee7878
Ilham Syauqi <ilhamsyauqih@outlook.co.id>
2026-08-26 23:58:33 +0700

    update random color progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Accordian from './components/accordion'
+import RandomColor from './components/random-color'
 
 function App() {
 
   return (
     <>
       {/* Accordian Component */}
-      <Accordian/>
+      {/* <Accordian/> */}
+
+      {/* Random Color Component */}
+      <RandomColor/>
     </>
   )
 }
