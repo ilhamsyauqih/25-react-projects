cdecdf753f37ba21f25e231493e188378fa4eb07
Ilham Syauqi <ilhamsyauqih@outlook.co.id>
2026-08-27 09:47:53 +0700

    menambahkan random color generator

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 export default function RandomColor(){
@@ -21,8 +21,17 @@ function handleCreateRandomHexColor() {
 }
 
 function handleCreateRandomRgbColor() {
+    const r = randomColorUtility(256)
+    const g = randomColorUtility(256)
+    const b = randomColorUtility(256)
 
+    setColor(`rgb(${r},${g},${b})`)
 }
+
+useEffect(()=>{
+    if(typeOfColor === "rgb") handleCreateRandomRgbColor()
+    else handleCreateRandomHexColor()
+}, [typeOfColor])
 
     return(
         <div style={{
@@ -33,6 +42,18 @@ function handleCreateRandomRgbColor() {
             <button onClick={()=>setTypeOfColor('hex')}>Create HEX Color</button>
             <button onClick={()=>setTypeOfColor('rgb')}>Create RGB Color</button>
             <button onClick={typeOfColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generate Random Color</button>
+            <div style={{
+                display : "flex",
+                justifyContent : "center",
+                alignItems : "center",
+                color : "#fff",
+                fontSize : "60px",
+                marginTop : "50px",
+                flexDirection : "column",
+            }}>
+                <h3>{typeOfColor === 'rgb' ? "RGB Color" : "HEX Color"}</h3>
+                <h1>{color}</h1>
+            </div>
         </div>
     )
 }
