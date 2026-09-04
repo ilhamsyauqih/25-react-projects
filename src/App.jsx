dc9bea22c3cd156b577e411b9638ffbbd05ec9fc
Ilham Syauqi <ilhamsyauqih@outlook.co.id>
2026-09-04 22:36:35 +0700

    add update in load more data component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Accordian from './components/accordion'
 import ImageSlider from './components/image-slider'
+import LoadMoreData from './components/load-more-data'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
 
@@ -18,7 +19,10 @@ function App() {
       {/* <StarRating noOfStars={10}/> */}
 
       {/* Image Slider Component */}
-      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/> */}
+
+      {/* Load More Products Components */}
+      <LoadMoreData />
     </>
   )
 }
--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function LoadMoreData(){
 
@@ -6,6 +6,31 @@ export default function LoadMoreData(){
     const [produtcs, setProducts] = useState([])
     const [count, setCount] = useState(0)
     
+    async function fetchProduts(){
+        try{
+            setLoading(true)
+            const response = await fetch(`https://dummyjson.com/products?limit=10&skip=${count === 0 ? 0 : count * 20}`)
+            const result = await response.json();
+
+            if(result && result.produtcs && result.produtcs.length){
+                setProducts(result.produtcs)
+                setLoading(false)
+            }
+            
+            
+        }catch(error){
+            console.log(error);
+            setLoading(false)
+        }
+    }
+
+    useEffect(()=>{
+        fetchProduts();
+    }, [])
+
+    if(loading){
+        return <div>Loading data! Please wait</div>
+    }
 
     return <div className="container">
         
