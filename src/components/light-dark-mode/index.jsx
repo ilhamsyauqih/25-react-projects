54fcb8dbbe68053548a3934667e456cacb32cdf6
Ilham Syauqi <ilhamsyauqih@outlook.co.id>
2026-09-13 10:46:08 +0700

    update adding light dark mode component

---
+++ src/components/light-dark-mode/index.jsx
@@ -0,0 +1,9 @@
+export default function LightDarkMode(){
+    return <div className="light-dark-mode">
+        <div className="container">
+            <p>Hello World!</p>
+            <button>Change Theme</button>
+
+        </div>
+    </div>
+}
